1205502f5223c2d72708f6d59b8334d11889039a
Amin Kouhouch <amin@MacBook-Air-de-Amin.local>
2025-10-16 12:12:55 +0200

    Ce qu'il reste à faire

---
+++ jeu.py
@@ -0,0 +1,13 @@
+""" Amin Kouhouch, Souleymane Ghamhi, 3ETI Jeudi 16 Octobre
+Ce fichier python contient le code du jeu du cassse briques
+
+Il reste à coder le programme principal mettant en relation les 4 autres classes
+"""
+
+import tkinter as tk
+import random
+
+class jeu:
+    def __init__(self):
+        self.
+
