ec97c368e67a5863fc6ef84f5c636f0f89e0bcd1
Amin Kouhouch <amin@MacBook-Air-de-Amin.local>
2025-10-16 12:13:42 +0200

    Affichage des briques commencé

--- affichage.py
+++ affichage.py
@@ -1,7 +1,7 @@
-"""Amin Kouhouch, 3ETI Jeudi 16 Octobre
+"""Amin Kouhouch, Souleymane Ghamhi, 3ETI Jeudi 16 Octobre
 Ce fichier contient la classe d'affichage du jeu du casse_brique
 
-Il reste à dessiner les briques et la raquette 
+Il reste à tester le programme pour voir les potentiels problèmes
 """
 import tkinter as tk
 
@@ -9,6 +9,7 @@ import tkinter as tk
 class Affichage:
 
     def __init__(self, largeur, hauteur):
+
         self.root = tk.Tk()
         self.canva = tk.Canvas(self.root, width = largeur, height = hauteur, bg = 'white')
         self.canva.pack()
@@ -20,6 +21,19 @@ class Affichage:
             balle.x + balle.radius, balle.y + balle.radius,
             fill = balle.color
             )
+        
+    def raquette_affichage(self, raquette):
+
+        self.raquette = self.canva.create_rectangle(
+            raquette.x, raquette.y,
+            raquette.x + raquette.width, raquette.y + raquette.height,
+            fill = raquette.color
+        )
     
+    def brique_affichage(self, brique):
+
+        for i in range(5):
+            for j in range (10):
+                brique.x = 60 + j * 70
 
 
