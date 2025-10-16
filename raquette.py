a2ec72f03ebd84e3fe6f19b2b79b7f05ec8bd315
Amin Kouhouch <amin@MacBook-Air-de-Amin.local>
2025-10-16 11:58:13 +0200

    Finalisation de la classe Raquette

--- raquette.py
+++ raquette.py
@@ -1,3 +1,19 @@
 """Amin Kouhouch 3ETI Jeudi 16 Octobre
 Ce fichier python contient la classe de la raquette
 """
+
+class Raquette:
+    def __init__(self, x, y, vx, hauteur, largeur, couleur):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.height = hauteur
+        self.width = largeur
+        self.color = couleur
+        self.direction = 0
+
+    def mouvement(self, largeur_c):
+
+        self.x += self.x * self.direction
+        if self.x < 0 or self.x > largeur_c:
+            self.direction = 0
