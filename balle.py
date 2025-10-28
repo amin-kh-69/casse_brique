cdf07c88fc2e285db0ab86107f38a650f6d02e5b
Souleymane <sghamhi@yahoo.com>
2025-10-28 11:55:21 +0100

    Clarification de l'entête Balle et ajouts de commentaire
    Updated class documentation and added comments for clarity.

--- balle.py
+++ balle.py
@@ -1,7 +1,16 @@
 """Amin Kouhouch, Souleymane Ghamhi, 3ETI Jeudi 16 Octobre
-Ce fichier contient la classe de la balle du jeu de casse briques
+Cette partie définit la classe Balle qui représente la balle du jeu de Casse-Brique.
+Elle gère les déplacements les rebonds sur les murs et la raquette ainsi que les
+collisions avec les briques à l’aide du canvas Tkinter.
 
+Fonctionnalités principales :
+- Mouvement automatique de la balle avec gestion des rebonds.
+- Détection des collisions avec les murs, la raquette et les briques.
+- Gestion de la perte de balle (sortie par le bas de l’écran).
 
+Il reste à :
+- Tester la précision des collisions avec la raquette notament les coter verticale de la raquette et les briques.
+- Donc ajuster la vitesse et les angles de rebond 
 """
 import random
 import tkinter as tk
@@ -14,12 +23,12 @@ class Balle:
         self.couleur = couleur
         self.id = self.canvas.create_oval(390, 350, 410, 370, fill=couleur)
         self.vx = random.choice([-3, 3])
-        self.vy = -3
-        self.lost = False
+        self.vy = -3  # la balle part vers le haut
+        self.lost = False    # Indique si la balle est perdue (tombée en bas)
 
     def move(self):
         self.canvas.move(self.id, self.vx, self.vy)
-        pos = self.canvas.coords(self.id)
+        pos = self.canvas.coords(self.id) # coordonnées actuelles de la balle
 
         # rebond sur murs
         if pos[0] <= 0 or pos[2] >= int(self.canvas['width']):
@@ -40,12 +49,12 @@ class Balle:
             if self._collision(brique.id):
                 self.vy = -self.vy
                 brique.detruire()
-                self.briques.remove(brique)
+                self.briques.remove(brique) # retire la brique de la liste
 
     def _collision(self, obj_id):
         pos_balle = self.canvas.coords(self.id)
-        pos_obj = self.canvas.coords(obj_id)
-        return not (pos_balle[2] < pos_obj[0] or pos_balle[0] > pos_obj[2] or pos_balle[3] < pos_obj[1] or pos_balle[1] > pos_obj[3])
+        pos_obj = self.canvas.coords(obj_id)    # Test de non-superposition des rectangles
+        return not (pos_balle[2] < pos_obj[0] or pos_balle[0] > pos_obj[2] or pos_balle[3] < pos_obj[1] or pos_balle[1] > pos_obj[3])  # balle à gauche a  droite en haut ou en bas de l'objet
 
 
 
