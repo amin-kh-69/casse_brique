5a78f9001f9c3aa8f9192be31918749bfe64bcdb
Amin Kouhouch <amin@MacBook-Air-de-Amin.local>
2025-10-30 17:47:57 +0100

    Ajout d'un bouton commandes qui donne les commandes

--- affichage.py
+++ affichage.py
@@ -34,11 +34,14 @@ class AffichageMenuAnimé:
         self.bouton_credits = tk.Button(self.root, text="Crédits", font=("Helvetica", 24), width=10, command=self.afficher_credits)
         self.bouton_jouer = tk.Button(self.root, text="Jouer", font=("Helvetica", 24), width=10, command=self.lancer_jeu)
         self.bouton_quitter = tk.Button(self.root, text="Quitter", font=("Helvetica", 24), width=10, command=self.root.destroy)
+        self.bouton_commandes = tk.Button(self.root, text="Commandes", font=("Helvetica", 24), width=10, command=self.afficher_commandes)
+
 
         # Position boutons sur le canvas
         self.canvas.create_window(400, 300, window=self.bouton_jouer)
         self.canvas.create_window(400, 350, window=self.bouton_credits)
         self.canvas.create_window(400, 400, window=self.bouton_quitter)
+        self.canvas.create_window(400, 450, window=self.bouton_commandes)
 
         # Création des briques animées
         self.briques = []
@@ -76,6 +79,13 @@ class AffichageMenuAnimé:
         self.canvas.delete("credits")  
         self.canvas.create_text(400, 550, text="AMIN KOUHOUCH / SOULEYMANE GHAMHI", font=("Helvetica", 24, "bold"), fill="white", tags="credits")
         self.root.after(3000, lambda: self.canvas.delete("credits"))
+    
+    def afficher_commandes(self):
+        # Affiche un label d'aide bien visible dans la fenêtre
+        label = tk.Label(self.root, text = "Pour déplacer la raquette ce sont les flèches\nPour utiliser vos bonus, c'est 'a'", font = ("Helvetica", 14), fg = 'black', bg = "light grey")
+        label_id = self.canvas.create_window(400, 500, window = label)  # 550 est visible dans le canvas 800x600
+        self.root.after(5000, lambda: self.canvas.delete(label_id))
+
 
 
 if __name__ == "__main__":
